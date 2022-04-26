92c4543f53fb45455abf1389cd6a74331a9ad418
Sardorbek Zokirov <sardorzakirov1999@gmail.com>
2022-04-26 10:48:15 +0200

    Enhanced the data

--- data_handler.py
+++ data_handler.py
@@ -23,14 +23,65 @@ from sklearn.linear_model import LogisticRegression
 
 data = pd.read_csv(r'data\heart.csv') 
 
+# (data.corr()['output'].sort_values().plot.barh())
+# (data.corr()['output'].abs().sort_values().plot.barh())
+# plt.show()
+"""
+exng       -0.436757
+oldpeak    -0.430696
+caa        -0.391724
+thall      -0.344029
+sex        -0.280937
+age        -0.225439
+trtbps     -0.144931
+chol       -0.085239
+fbs        -0.028046
+restecg     0.137230
+slp         0.345877
+thalachh    0.421741
+cp          0.433798
+output      1.000000
+"""
 
+# Build a data enhancer
 
+def data_enhance(data):
+    org_data = data
+    for sex in data['sex'].unique():
+        sex_data = org_data[org_data['sex']==sex]
+        age_std = sex_data['age'].std()
+        trtbps_std = sex_data['trtbps'].std()
+        chol_std = sex_data['chol'].std()
+        thalachh_std = sex_data['thalachh'].std()
+        oldpeak_std = sex_data['oldpeak'].std()
+        for i in org_data[org_data['sex']==sex].index:
+            if np.random.randint(2) == 1:
+                org_data['age'].values[i] += age_std/10
+                org_data['trtbps'].values[i] += trtbps_std/10
+                org_data['chol'].values[i] += chol_std/10
+                org_data['thalachh'].values[i] += thalachh_std/10
+                org_data['oldpeak'].values[i] += oldpeak_std/10
+            else:
+                org_data['age'].values[i] -= age_std/10
+                org_data['trtbps'].values[i] -= trtbps_std/10
+                org_data['chol'].values[i] -= chol_std/10
+                org_data['thalachh'].values[i] -= thalachh_std/10
+                org_data['oldpeak'].values[i] -= oldpeak_std/10
+
+    return org_data
+
+gen = data_enhance(data)
 x = data.drop(['output'], axis=1) # features - train and val data
 y = data['output'] # target
 
 num_vals = ['age', 'trtbps','thalachh', 'chol', 'oldpeak']
 cat_vals = ['sex', 'cp', 'exng', 'slp', 'caa', 'thall', 'restecg', 'fbs']
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Add enhanced data to 20% of the orig data
+enhanced_sample = gen.sample(gen.shape[0] // 5)
+x_train = pd.concat([x_train, enhanced_sample.drop(['output'], axis=1 ) ])
+y_train = pd.concat([y_train, enhanced_sample['output'] ])
 
 
 num_pipeline = Pipeline([
@@ -81,6 +132,15 @@ results_order = results.sort_values(by=['Accuracy Score'], ascending=False, igno
 
 print(results_order)
 
+def predictor(features):
+
+    best_model = classifiers.get("Extra Trees")
+
+    best_model.fit(x_train, y_train)
+
+    preds = best_model.predict(features)
+    return preds
+
 # Benchmark
 """
                  Model  Accuracy Score  Balanced Accuracy score      Time
@@ -93,4 +153,18 @@ print(results_order)
 6    Gradient Boosting       80.327869                80.065359  0.076796
 7              XGBoost       78.688525                78.594771  0.131649
 8        Decision Tree       75.409836                75.653595  0.012834
+"""
+
+# STD
+"""
+                 Model  Accuracy Score  Balanced Accuracy score      Time
+0            Ada Boost       91.803279                92.265795  0.133611
+1        Random Forest       88.524590                88.943355  0.129669
+2          Extra Trees       88.524590                88.562092  0.109739
+3             Catboost       88.524590                88.562092  0.451954
+4             LightGBM       86.885246                87.091503  0.091729
+5    Gradient Boosting       85.245902                85.620915  0.090088
+6              XGBoost       85.245902                85.239651  0.131679
+7  Logistic Regression       83.606557                83.006536  0.047480
+8        Decision Tree       80.327869                80.827887  0.012487
 """
